b51983861e4ad81a28f8f24f6569a86c386d369b
haramsong <alakazam1324@gmail.com>
2023-10-26 16:47:58 +0900

    settings locale 변경

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,10 +44,6 @@ INSTALLED_APPS = [
     'rest_framework'
 ]
 
-LANGUAGE_CODE = 'ko'
-
-TIME_ZONE = 'Asia/Seoul'
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -115,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
